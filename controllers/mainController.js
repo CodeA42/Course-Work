ae4e0b3c15670f8349c7bd2bc6dd187eee126e69
Aleksandar Velichkov <sashko98@gmail.com>
2021-02-25 10:50:30 +0200

    home page now displays articles from the db

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -5,10 +5,12 @@ const mongoose = require('mongoose');
 router.get('/', async (req, res) => {
   const ArticleModel = mongoose.model('ArticleModel');
 
-  const found = await ArticleModel.find({});
-  console.log(found);
+  const articleCount = await ArticleModel.find().estimatedDocumentCount();
+  const page = req.query.p ?? 0;
+  const articles = await ArticleModel.find().limit(5).skip(page * 5);
+  const article = articles.map(e => e.toObject());
 
-  res.render('home', {title: 'Home Page'});
+  res.render('home', {title: 'Home Page', article});
 });
 
 router.get('/register', (req, res) => {
