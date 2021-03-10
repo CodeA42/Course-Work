1aa2552a73c3d91b057be9c5f6a6cb73856716df
Aleksandar Velichkov <sashko98@gmail.com>
2021-03-10 15:33:57 +0200

    -added chekAuthenticated -added checkNotAuthenticated

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,6 +2,23 @@ const bodyParser = require('body-parser');
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+function checkAuthenticated(req, res, next) {
+  if(req.isAuthenticated()) {
+    return next();
+  }
+
+  res.redirect('/login');
+}
+
+function checkNotAuthenticated(req, res, next) {
+  if(req.isAuthenticated()) {
+    return res.redirect('/');
+  }
+  next();
+}
+
 module.exports = {
   urlencodedParser,
+  checkAuthenticated,
+  checkNotAuthenticated
 };
