2cc77df48e6095e3cf0f487e261520acf2b832dd
Aleksandar Velichkov <sashko98@gmail.com>
2021-03-11 01:35:54 +0200

    -added commentCard

--- config/partials.js
+++ config/partials.js
@@ -5,3 +5,6 @@ handlebars.registerPartial('userCard', userCard);
 
 const articleCard = require('../views/article/card');
 handlebars.registerPartial('articleCard', articleCard);
+
+const commentCard = require('../views/general/comment/card');
+handlebars.registerPartial('commentCard', commentCard);
