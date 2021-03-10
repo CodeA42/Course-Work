ba0c1e747d84cca4644e5f7288424c5dceb153d0
Aleksandar Velichkov <sashko98@gmail.com>
2021-03-10 23:37:11 +0200

    -added comments to article

--- config/db/models/ArticleModel.js
+++ config/db/models/ArticleModel.js
@@ -4,12 +4,17 @@ const authorsSchema = mongoose.Schema({
   id: String
  }, {_id:false});
 
+const commentSchema = mongoose.Schema({
+  id: String
+}, {_id: false})
+
 const articleSchema = new mongoose.Schema({
  title: String,
  body: String,
  postDate: {type: Date, default: Date.now},
  authors: [authorsSchema],
- postedBy: String
+ postedBy: String,
+ comments: [commentSchema]
 });
 
 mongoose.model('ArticleModel', articleSchema);
